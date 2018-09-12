af6589fdfd357ad57617a640f8f2ef30d0231801
ariebrainware <ariebrainware@gmail.com>
2018-09-12 01:05:00 -0400

    Add Router url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './large/Home'
+import Dashboard from './large/Dashboard'
+
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/user/dashboard' component={Dashboard} />
+				</Switch>
+			</Router>			
+		)
+	}
 }
 
-export default App;
+export default App
